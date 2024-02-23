900768770ac78a2dd9053b443d87337fb8c9773e
Shubham <jkmeventsofficial@gmail.com>
2024-02-23 15:48:45 +0530

    adding domain

--- JKM/deployment.py
+++ JKM/deployment.py
@@ -2,9 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], "https://webapp.jkmevents.in"]
 
-CSRF_TRUSTED_ORIGINS = ["https://"+ os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ["https://"+ os.environ['WEBSITE_HOSTNAME'], "https://webapp.jkmevents.in"]
 
 DEBUG = True
 
