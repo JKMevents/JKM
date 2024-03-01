601675524eb5ebc7fddb2e351ed313c8ad46333c
Shubham <jkmeventsofficial@gmail.com>
2024-03-01 11:34:40 +0530

    Making the system faster 2

--- JKMapp/utils.py
+++ JKMapp/utils.py
@@ -111,43 +111,47 @@ def add_text_to_image(canvas, number, ticket_template, text_info, font_path, fon
 
 
 
-
 def create_tickets(number, ticket_template, margin =10, data_prefix="JKM2024", font_path = "JKMapp/static/KodeMono-Regular.ttf"):
-    ticket_width, ticket_height = ticket_template.size
-    total_width = ticket_width
-    total_height = (number * ticket_height) +  margin
-    canvas = Image.new('RGB', (total_width, total_height), color='white')
-    y_offset = 0
-    # Redirect to the same view after processing the form submission
-    
-
+    # 1. Define prerequisites and variables
+    #font_path = "KodeMono-Regular.ttf"
     font_size = 50
     color = (255, 0, 0)
+    text_info = []
 
-        # Get current date and time
-    current_datetime = datetime.datetime.now()
-
-        # Format the datetime object as a string
+    # Get current date and time
+    current_datetime = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30)
     formatted_date = current_datetime.strftime("%d-%m-%Y")
     formatted_time = current_datetime.strftime("%H:%M:%S")
     ticket_number = "www.JKMevents.in"
 
-        # Define text and positions
-    text_info = [
-            (f"{formatted_date}", (200, 350)),
-            (f"{formatted_time}", (700, 470)),
-            ("Rs.100", (1250, 470)),
-            (f"{ticket_number}", (1500, 470))
-        ]
 
-    draw = ImageDraw.Draw(canvas)
+    # Populate text information
+    text_info.append((f"{formatted_date}", (200, 350)))
+    text_info.append((f"{formatted_time}", (700, 470)))
+    text_info.append(("Rs.100", (1250, 470)))
+    text_info.append((f"{ticket_number}", (1500, 470)))
 
-    # Create a font object.
+
+    # 2. Make a copy of the ticket template
+    ticket_template_copy = ticket_template.copy()
+
+
+    # 3. Run the first loop to paste text information
+    draw = ImageDraw.Draw(ticket_template_copy)
     font = ImageFont.truetype(font_path, size=font_size)
+    for text, position in text_info:
+        draw.text(position, text, fill=color, font=font)
 
-    for i in range(1, number+1):
-        canvas.paste(ticket_template, (0, y_offset))
-
+    # 4. Create the canvas
+    ticket_width, ticket_height = ticket_template_copy.size
+    total_width = ticket_width
+    total_height = (number * ticket_height) + margin
+    canvas = Image.new('RGB', (total_width, total_height), color='white')
+    
+    
+    y_offset = 0
+    
+    for i in range(1, number + 1):
         qr = qrcode.QRCode(
             version=1,
             box_size=10,
@@ -157,16 +161,15 @@ def create_tickets(number, ticket_template, margin =10, data_prefix="JKM2024", f
         qr.add_data(qr_data)
         qr.make(fit=True)
         qr_img = qr.make_image(fill_color="black", back_color="white")
+
+        # 6. Paste the QR code onto the ticket template copy    
         qr_width, qr_height = qr_img.size
         qr_x_offset = ticket_width - qr_width - margin
         qr_y_offset = y_offset + (ticket_height - qr_height) // 2
-        canvas.paste(qr_img, (qr_x_offset, qr_y_offset))
+        ticket_template_copy.paste(qr_img, (qr_x_offset, qr_y_offset))
 
-        for text, position in text_info:
-            draw.text((position[0], position[1] + (ticket_height+margin)*(i-1) ), text, fill=color, font=font)
+        # 7. Paste the ticket template copy onto the canvas
+        canvas.paste(ticket_template_copy, (0, (ticket_height + margin) * (i - 1)))
 
-
-        y_offset += ticket_height + margin
-
+     # 8. Return the canvas
     return canvas
-
